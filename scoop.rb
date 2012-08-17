28e4eb0d20f7633745cfe4499a1718a3890cdcc6
dan simpson <danorine@gmail.com>
2012-08-17 20:52:45 -0300

    Update scoop.rb

--- scoop.rb
+++ scoop.rb
@@ -3,8 +3,9 @@ require 'pipe'
 require 'sinatra'
 
   get '/' do
-    Pipe.create do
-      feed "http://www.oxfamblogs.org/fp2p/?feed=rss2"
-      feed "http://chronicle.com/blognetwork/edgeofthewest/feed"
-    end
+    "Well, ok then!"
+    #Pipe.create do
+    #  feed "http://www.oxfamblogs.org/fp2p/?feed=rss2"
+    #  feed "http://chronicle.com/blognetwork/edgeofthewest/feed"
+    #end
   end
